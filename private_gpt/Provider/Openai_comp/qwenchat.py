3ddcbfc9345e9001e8352b6f8e1953020b4f7ff5
Hacker-Sam-is-here <jrdowney006@gmail.com>
2026-03-21 18:55:34 +0530

    More models

--- private_gpt/Provider/Openai_comp/qwenchat.py
+++ private_gpt/Provider/Openai_comp/qwenchat.py
@@ -380,12 +380,22 @@ class Chat(BaseChat):
 class QwenChat(OpenAICompatibleProvider):
     name = "QwenChat"
     AVAILABLE_MODELS = [
+        "qwen3-max-preview",
+        "qwen-plus-2025-09-11",
         "qwen3-235b-a22b",
+        "qwen3-coder-plus",
+        "qwen3-30b-a3b",
+        "qwen3-coder-30b-a3b-instruct",
+        "qwen-max-latest",
         "qwen-plus-2025-01-25",
         "qwq-32b",
         "qwen-turbo-2025-02-11",
         "qwen2.5-omni-7b",
+        "qvq-72b-preview-0310",
         "qwen2.5-vl-32b-instruct",
+        "qwen2.5-14b-instruct-1m",
+        "qwen2.5-coder-32b-instruct",
+        "qwen2.5-72b-instruct",
     ]
     required_auth = False
 
